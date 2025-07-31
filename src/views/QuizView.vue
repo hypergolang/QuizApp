bd440bd1f0b728d1840a7e34872e0ae650e2ffae
hypergolang <hypergolang>
2025-07-31 21:15:36 +0700

    refactor(quiz): use computed property for question status

--- src/views/QuizView.vue
+++ src/views/QuizView.vue
@@ -2,7 +2,7 @@
     import Question from "../components/Question.vue"
     import QuizHeader from "@/components/QuizHeader.vue";
     import { useRoute } from "vue-router";
-    import { ref, watch } from "vue";
+    import { computed, ref, watch } from "vue";
     import quizes from "../data/quizes.json"
 
     const route = useRoute()
@@ -13,13 +13,15 @@
 
     const currentQuestionIndex = ref(0)
 
-    const questionStatus = ref(`${currentQuestionIndex.value}/${quiz.questions.length}`)
+    // const questionStatus = ref(`${currentQuestionIndex.value}/${quiz.questions.length}`)
 
-    watch(() => currentQuestionIndex.value, () => {
-        questionStatus.value = `${currentQuestionIndex.value}/${quiz.questions.length}`
-    })
+    // watch(() => currentQuestionIndex.value, () => {
+    //     questionStatus.value = `${currentQuestionIndex.value}/${quiz.questions.length}`
+    // })
 
-</script>``
+    const questionStatus = computed(() => `${currentQuestionIndex.value}/${quiz.questions.length}`)
+
+</script>
 
 <template>
     <div>
