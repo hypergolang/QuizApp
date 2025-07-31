f0f960dd67cae66a715f9d9b2c1a49bdf5b17cf4
hypergolang <hypergolang>
2025-07-31 21:12:05 +0700

    feat: Implement dynamic question status and next question navigation

--- src/views/QuizView.vue
+++ src/views/QuizView.vue
@@ -1,8 +1,8 @@
 <script setup>
     import Question from "../components/Question.vue"
     import QuizHeader from "@/components/QuizHeader.vue";
-    import { ref } from "vue";
     import { useRoute } from "vue-router";
+    import { ref, watch } from "vue";
     import quizes from "../data/quizes.json"
 
     const route = useRoute()
@@ -12,13 +12,21 @@
     const quiz = quizes.find(q => q.id === quizId)
 
     const currentQuestionIndex = ref(0)
-</script>
+
+    const questionStatus = ref(`${currentQuestionIndex.value}/${quiz.questions.length}`)
+
+    watch(() => currentQuestionIndex.value, () => {
+        questionStatus.value = `${currentQuestionIndex.value}/${quiz.questions.length}`
+    })
+
+</script>``
 
 <template>
     <div>
-        <QuizHeader />
+        <QuizHeader :questionStatus="questionStatus" />
         <div>
             <Question :question="quiz.questions[currentQuestionIndex]" />
         </div>
+        <button @click="currentQuestionIndex++">Next Question</button>
     </div>
 </template>
